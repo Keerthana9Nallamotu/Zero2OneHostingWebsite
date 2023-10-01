79ebdf4a7e68dcfc9f749b61bc4f054f68155fb6
KeerthanaNallamotu <keerthana.nallamotu@gmail.com>
2023-10-01 05:52:12 -0500

    Fixed login

--- api/app.py
+++ api/app.py
@@ -108,7 +108,7 @@ def login():
                 print('HASHED: ', str(bcrypt.generate_password_hash(password))[1:])
                 if account:
                     print(account)
-                    if bcrypt.check_password_hash(password, account[0]):
+                    if bcrypt.check_password_hash(account[0], password):
                         print('parse: ', parser.parse(account[1]))
 
                         session['loggedin'] = True
